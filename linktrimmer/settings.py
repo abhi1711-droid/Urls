096f839f345b6ba711c0562f51f0143737091f53
Abhishek Kumar <abhisheksinha1709@gmail.com>
2022-07-06 18:55:53 +0530

    changes to settings.py

--- linktrimmer/settings.py
+++ linktrimmer/settings.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 
-import dj_database_url
 import environ
 
 
@@ -28,7 +27,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
-    'whitenoise.runserver_nostatic',
 ]
 
 AUTH_USER_MODEL = 'users.CustomUserModel'
@@ -77,8 +75,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
