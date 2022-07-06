7489a7cff24c01d2eb1dad423640b88ac1736068
Abhishek Kumar <abhisheksinha1709@gmail.com>
2022-07-06 21:46:00 +0530

    added seetings for postgres

--- linktrimmer/settings.py
+++ linktrimmer/settings.py
@@ -66,6 +66,11 @@ DATABASES = {
     'default': {
         'ENGINE': env('ENGINE'),
         'NAME': env('NAME'),
+        'USER': env('USER'),
+        'PASSWORD': env('PASSWORD'),
+        'HOST': env('HOST'),
+        'PORT': env('PORT'),
+
     }
 }
 
